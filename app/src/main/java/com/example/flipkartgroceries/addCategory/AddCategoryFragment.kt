99a728954b5ae812f4e03f2f9e168b0f4c60f788
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-11-30 13:21:18 +0530

    image picker added in add categories page

--- app/src/main/java/com/example/flipkartgroceries/addCategory/AddCategoryFragment.kt
+++ app/src/main/java/com/example/flipkartgroceries/addCategory/AddCategoryFragment.kt
@@ -1,16 +1,46 @@
 package com.example.flipkartgroceries.addCategory
 
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.activity.result.ActivityResultLauncher
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.flipkartgroceries.R
 import com.example.flipkartgroceries.base.BaseFragment
 import com.example.flipkartgroceries.databinding.FragmentAddCategoryBinding
+import kotlinx.coroutines.flow.collectLatest
 
 class AddCategoryFragment : BaseFragment<FragmentAddCategoryBinding, AddCategoryViewModel>() {
-    override fun getViewModel()= AddCategoryViewModel::class.java
+    override fun getViewModel() = AddCategoryViewModel::class.java
+    lateinit var getContent: ActivityResultLauncher<String>
+    override fun getLayoutResource() = R.layout.fragment_add_category
 
-    override fun getLayoutResource()= R.layout.fragment_add_category
-
+    //    private val pickPicture= registerForActivityResult(ActivityResultContracts.GetContent())
     override fun setUp() {
-        dataBinding.viewModel=viewModel
+        dataBinding.viewModel = viewModel
+        lifecycleScope.launchWhenResumed {
+            viewModel.selectImageEvent.collectLatest {
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.type = "image/*"
+                startActivityForResult(intent, 456)
+            }
+        }
+        lifecycleScope.launchWhenResumed {
+            viewModel.submitBtnEvent.collectLatest {
+                val action =
+                    AddCategoryFragmentDirections.actionAddCategoryFragmentToViewCategoriesFragment()
+                findNavController().navigate(action)
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 123) {
+            var bitmap = data?.extras?.get("data ") as Bitmap
+//            dataBinding.selectImg.setImageBitMap
+        }
     }
 
 
--- app/src/main/java/com/example/flipkartgroceries/addCategory/AddCategoryViewModel.kt
+++ app/src/main/java/com/example/flipkartgroceries/addCategory/AddCategoryViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.flipkartgroceries.addCategory
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 
 class AddCategoryViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val selectImageEventChannel = Channel<Unit>()
+    val selectImageEvent = selectImageEventChannel.receiveAsFlow()
+
+    private val submitBtnEventChannel = Channel<Unit>()
+    val submitBtnEvent = submitBtnEventChannel.receiveAsFlow()
+
+    fun selectImage(){
+        selectImageEventChannel.trySend(Unit)
+    }
+
+    fun submitCategory(){
+        submitBtnEventChannel.trySend(Unit)
+    }
 }
