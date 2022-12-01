a6e906e1cc0852d7507246530cda74ff449d0d64
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-12-01 18:10:38 +0530

    Hilt implemented

--- app/src/main/java/com/example/flipkartgroceries/addCategory/AddCategoryViewModel.kt
+++ app/src/main/java/com/example/flipkartgroceries/addCategory/AddCategoryViewModel.kt
@@ -1,21 +1,44 @@
 package com.example.flipkartgroceries.addCategory
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flipkartgroceries.database.AppDataBase
+import com.example.flipkartgroceries.database.CategoriesEntity
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class AddCategoryViewModel : ViewModel() {
+@HiltViewModel
+class AddCategoryViewModel @Inject constructor(private val groceries: AppDataBase) : ViewModel() {
     private val selectImageEventChannel = Channel<Unit>()
     val selectImageEvent = selectImageEventChannel.receiveAsFlow()
+
+    val categoryId = MutableStateFlow("")
+    val categoryImage = MutableStateFlow("")
+    val categoryName = MutableStateFlow("")
+
 
     private val submitBtnEventChannel = Channel<Unit>()
     val submitBtnEvent = submitBtnEventChannel.receiveAsFlow()
 
-    fun selectImage(){
+    fun selectImage() {
         selectImageEventChannel.trySend(Unit)
     }
 
-    fun submitCategory(){
-        submitBtnEventChannel.trySend(Unit)
+    fun submitCategory() {
+        viewModelScope.launch(Dispatchers.IO) {
+            groceries.categoriesDao().insertCategories(
+                CategoriesEntity(
+                    categoryId = categoryId.value,
+                    categoryImage = categoryImage.value,
+                    categoryName = categoryName.value
+                )
+            )
+            submitBtnEventChannel.trySend(Unit)
+        }
     }
 }
