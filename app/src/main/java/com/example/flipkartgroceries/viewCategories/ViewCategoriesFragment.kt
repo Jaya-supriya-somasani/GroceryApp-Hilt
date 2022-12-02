fb4a31234865190d40b0399a4d2baf31c8c56d7d
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-12-02 16:21:51 +0530

    Adapter is attached to the fragment and retrieving categories details from database

--- app/src/main/java/com/example/flipkartgroceries/viewCategories/ViewCategoriesFragment.kt
+++ app/src/main/java/com/example/flipkartgroceries/viewCategories/ViewCategoriesFragment.kt
@@ -1,21 +1,30 @@
 package com.example.flipkartgroceries.viewCategories
 
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.flipkartgroceries.R
 import com.example.flipkartgroceries.base.BaseFragment
 import com.example.flipkartgroceries.databinding.FragmentViewCategoriesBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class ViewCategoriesFragment :
     BaseFragment<FragmentViewCategoriesBinding>() {
     private val viewCategoryViewModel: ViewCategoriesViewModel by viewModels()
 
-    override fun getLayoutResource()=R.layout.fragment_view_categories
+    override fun getLayoutResource() = R.layout.fragment_view_categories
 
-    val adapter=ViewCategoryAdapter()
+    var adapter = ViewCategoryAdapter()
 
     override fun setUp() {
-        dataBinding.viewModel=viewCategoryViewModel
+        dataBinding.viewModel = viewCategoryViewModel
+        dataBinding.recyclerView.adapter=adapter
+
+        lifecycleScope.launchWhenResumed {
+            viewCategoryViewModel.list.collectLatest {
+                adapter.submitList(it)
+            }
+        }
     }
-
-
 }
--- app/src/main/java/com/example/flipkartgroceries/viewCategories/ViewCategoriesViewModel.kt
+++ app/src/main/java/com/example/flipkartgroceries/viewCategories/ViewCategoriesViewModel.kt
@@ -1,7 +1,26 @@
 package com.example.flipkartgroceries.viewCategories
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flipkartgroceries.database.AppDataBase
+import com.example.flipkartgroceries.database.CategoriesEntity
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ViewCategoriesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class ViewCategoriesViewModel @Inject constructor(private val groceries: AppDataBase) :
+    ViewModel() {
+    var list= MutableStateFlow(listOf<CategoriesEntity>())
+
+    init {
+        getCategoriesData()
+    }
+    fun getCategoriesData() {
+        viewModelScope.launch(Dispatchers.IO) {
+            list.value = groceries.categoriesDao().getAllCategories()
+        }
+    }
 }
