259a7dff25a03fbeda953398ce46e879a664de68
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-12-02 16:20:35 +0530

    Glider is attached to the view category adapter

--- app/src/main/java/com/example/flipkartgroceries/viewCategories/ViewCategoryAdapter.kt
+++ app/src/main/java/com/example/flipkartgroceries/viewCategories/ViewCategoryAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.flipkartgroceries.viewCategories
 
+import android.util.Log
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.flipkartgroceries.R
 import com.example.flipkartgroceries.base.BaseAdapter
 import com.example.flipkartgroceries.base.BaseViewHolder
@@ -8,7 +10,8 @@ import com.example.flipkartgroceries.base.inflate
 import com.example.flipkartgroceries.database.CategoriesEntity
 import com.example.flipkartgroceries.databinding.ItemCategoriesBinding
 
-class ViewCategoryAdapter : BaseAdapter<CategoriesEntity>() {
+class ViewCategoryAdapter
+    : BaseAdapter<CategoriesEntity>() {
 
     lateinit var binding: ItemCategoriesBinding
     override fun onCreateViewHolder(
@@ -21,7 +24,14 @@ class ViewCategoryAdapter : BaseAdapter<CategoriesEntity>() {
     inner class ViewCategoryViewHolder(binding: ItemCategoriesBinding) :
         BaseViewHolder<ItemCategoriesBinding, CategoriesEntity>(binding) {
         override fun onBind(item: CategoriesEntity) {
-            binding.items=item
+            try {
+                binding.items = item
+                Glide.with(binding.imageVw.context).load(item.categoryImage)
+                    .placeholder(R.drawable.fruits)
+                    .error(R.drawable.fruits).into(binding.imageVw)
+            } catch (exception: Exception) {
+                Log.d("TAG", "Unable to load image")
+            }
         }
     }
 }
