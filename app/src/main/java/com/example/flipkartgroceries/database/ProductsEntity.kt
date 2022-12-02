cfc5a699a728e529705736520a972d5bb00187ef
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-12-02 16:00:42 +0530

    Auto generated primary key added

--- app/src/main/java/com/example/flipkartgroceries/database/ProductsEntity.kt
+++ app/src/main/java/com/example/flipkartgroceries/database/ProductsEntity.kt
@@ -6,11 +6,12 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class ProductsEntity(
-    @PrimaryKey val categoryName:String,
+    @PrimaryKey(autoGenerate = true) val productId:Int,
+    @ColumnInfo(name = "category_name") val categoryName:String,
     @ColumnInfo(name = "product_name") val productName:String?,
     @ColumnInfo(name = "product_weight") val productWeight:String?,
-    @ColumnInfo(name = "product_mrp") val productMRP:Int,
-    @ColumnInfo(name = "product_price") val productPrice:Int,
+    @ColumnInfo(name = "product_mrp") val productMRP:String,
+    @ColumnInfo(name = "product_price") val productPrice:String,
     @ColumnInfo(name = "description") val description:String,
     @ColumnInfo(name = "product_image") val productImage:String
 )
