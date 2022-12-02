13036d30e09e2ae05f53572e21f3708428b6f147
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-12-02 16:00:27 +0530

    Retrieving all products from product entity query implemented

--- app/src/main/java/com/example/flipkartgroceries/database/ProductsDao.kt
+++ app/src/main/java/com/example/flipkartgroceries/database/ProductsDao.kt
@@ -2,11 +2,12 @@ package com.example.flipkartgroceries.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface ProductsDao {
-//    @Query("Select * from productsentity")
-//    fun getAllProducts():List<ProductsDao>
+    @Query("Select * from productsentity")
+    fun getAllProducts():List<ProductsEntity>
     @Insert
     fun insertProducts(vararg products:ProductsEntity)
 }
