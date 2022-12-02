c38fe408a5b287a5d01250f46378717f1bb924e6
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-12-02 16:19:53 +0530

    Adapter is atatched to the view products fragment

--- app/src/main/java/com/example/flipkartgroceries/viewProducts/ViewProductsFragment.kt
+++ app/src/main/java/com/example/flipkartgroceries/viewProducts/ViewProductsFragment.kt
@@ -1,15 +1,26 @@
 package com.example.flipkartgroceries.viewProducts
 
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.flipkartgroceries.R
 import com.example.flipkartgroceries.base.BaseFragment
 import com.example.flipkartgroceries.databinding.FragmentViewProductsBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class ViewProductsFragment : BaseFragment<FragmentViewProductsBinding>() {
     private val viewProductsViewModel : ViewProductsViewModel by viewModels()
     override fun getLayoutResource() = R.layout.fragment_view_products
+    var adapter=ViewProductsAdapter()
 
     override fun setUp() {
         dataBinding.viewModel = viewProductsViewModel
+        dataBinding.recyclerView.adapter=adapter
+        lifecycleScope.launchWhenResumed {
+            viewProductsViewModel.productsList.collectLatest {
+                adapter.submitList(it)
+            }
+        }
     }
 }
