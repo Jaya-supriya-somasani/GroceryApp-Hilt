c438a4467c360214de774581482e0d1708a89674
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-12-02 10:17:58 +0530

    View category adapter is created

--- app/src/main/java/com/example/flipkartgroceries/viewCategories/ViewCategoryAdapter.kt
+++ app/src/main/java/com/example/flipkartgroceries/viewCategories/ViewCategoryAdapter.kt
@@ -1,43 +1,27 @@
 package com.example.flipkartgroceries.viewCategories
 
-import android.net.Uri
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.RecyclerView
 import com.example.flipkartgroceries.R
-import com.example.flipkartgroceries.database.ViewCategoryItems
+import com.example.flipkartgroceries.base.BaseAdapter
+import com.example.flipkartgroceries.base.BaseViewHolder
+import com.example.flipkartgroceries.base.inflate
+import com.example.flipkartgroceries.database.CategoriesEntity
 import com.example.flipkartgroceries.databinding.ItemCategoriesBinding
 
-class ViewCategoryAdapter() :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    lateinit var items: List<ViewCategoryItems>
+class ViewCategoryAdapter : BaseAdapter<CategoriesEntity>() {
+
     lateinit var binding: ItemCategoriesBinding
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        binding = DataBindingUtil.inflate(
-            LayoutInflater.from(parent.context),
-            R.layout.item_categories,
-            parent,
-            false
-        )
-        return ViewHolder(binding.root)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewCategoryViewHolder {
+        return ViewCategoryViewHolder(parent.inflate(R.layout.item_categories))
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        binding.imageVw.setImageURI(Uri.parse(items[position].image))
-        binding.categoryNameTv.text = items[position].categoryName
-    }
-
-    override fun getItemCount(): Int {
-        return items.size
-    }
-
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-
-    }
-    fun submitList(items:List<ViewCategoryItems>){
-        this.items=items
+    inner class ViewCategoryViewHolder(binding: ItemCategoriesBinding) :
+        BaseViewHolder<ItemCategoriesBinding, CategoriesEntity>(binding) {
+        override fun onBind(item: CategoriesEntity) {
+            binding.items=item
+        }
     }
 }
