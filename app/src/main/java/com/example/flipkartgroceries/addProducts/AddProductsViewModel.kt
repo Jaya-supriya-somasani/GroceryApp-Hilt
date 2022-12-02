653acbd2dbdaafdf621c70fb8fe0c89a03aabcb3
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-12-02 16:13:24 +0530

    1.Products details is inserted to the database 2.toast event is sending while adding the data to the database

--- app/src/main/java/com/example/flipkartgroceries/addProducts/AddProductsViewModel.kt
+++ app/src/main/java/com/example/flipkartgroceries/addProducts/AddProductsViewModel.kt
@@ -1,14 +1,62 @@
 package com.example.flipkartgroceries.addProducts
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flipkartgroceries.database.AppDataBase
+import com.example.flipkartgroceries.database.ProductsEntity
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class AddProductsViewModel : ViewModel() {
-    private val addProductEventChannel=Channel<Unit>()
-    val addProductEvent=addProductEventChannel.receiveAsFlow()
+@HiltViewModel
+class AddProductsViewModel @Inject constructor(private val groceries: AppDataBase) : ViewModel() {
+    private val addProductEventChannel = Channel<Unit>()
+    val addProductEvent = addProductEventChannel.receiveAsFlow()
+    val categoryType = MutableStateFlow("")
+    val productName = MutableStateFlow("")
+    val productWeight = MutableStateFlow("")
+    val productMRP = MutableStateFlow("")
+    val productPrice = MutableStateFlow("")
+    val productDescription = MutableStateFlow("")
+    val selectProductImage = MutableStateFlow("")
+    val productId=MutableStateFlow(0)
 
-    fun addProductBtnClicked(){
-        addProductEventChannel.trySend(Unit)
+    private val selectImageChannelEvent = Channel<Unit>()
+    val selectImageEvent = selectImageChannelEvent.receiveAsFlow()
+
+    private val toastChannelEvent=Channel<String>()
+    val toastEvent=toastChannelEvent.receiveAsFlow()
+
+    fun selectProductImage() {
+        selectImageChannelEvent.trySend(Unit)
+    }
+
+
+    fun addProductBtnClicked() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                groceries.productsDao().insertProducts(
+                    ProductsEntity(
+                        categoryName = categoryType.value,
+                        productName = productName.value,
+                        productWeight = productWeight.value,
+                        productMRP = productMRP.value,
+                        productPrice = productPrice.value,
+                        description = productDescription.value,
+                        productImage = selectProductImage.value,
+                        productId=productId.value
+                    )
+                )
+                addProductEventChannel.trySend(Unit)
+                toastChannelEvent.trySend("Product details added Successfully")
+            }
+            catch (exception:Exception){
+                toastChannelEvent.trySend("Unable to add product details")
+            }
+        }
     }
 }
