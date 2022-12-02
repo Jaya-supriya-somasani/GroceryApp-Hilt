ddbb1ee8957207500c4009d02ebeabbe41b31810
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-12-02 16:19:09 +0530

    Retrieving all products details from database

--- app/src/main/java/com/example/flipkartgroceries/viewProducts/ViewProductsViewModel.kt
+++ app/src/main/java/com/example/flipkartgroceries/viewProducts/ViewProductsViewModel.kt
@@ -1,7 +1,24 @@
 package com.example.flipkartgroceries.viewProducts
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flipkartgroceries.database.AppDataBase
+import com.example.flipkartgroceries.database.ProductsEntity
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ViewProductsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class ViewProductsViewModel @Inject constructor(private val groceries: AppDataBase) : ViewModel() {
+    var productsList=MutableStateFlow(listOf<ProductsEntity>())
+    init {
+        getProducts()
+    }
+    private fun getProducts(){
+        viewModelScope.launch(Dispatchers.IO) {
+            productsList.value=groceries.productsDao().getAllProducts()
+        }
+    }
 }
