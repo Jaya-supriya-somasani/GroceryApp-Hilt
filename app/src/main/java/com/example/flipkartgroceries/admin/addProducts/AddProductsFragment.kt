c9cc0a999c738fedf815e24b2d9a6c332f47c15d
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-12-07 15:50:15 +0530

    1.Navigation args implemented to add products

--- app/src/main/java/com/example/flipkartgroceries/admin/addProducts/AddProductsFragment.kt
+++ app/src/main/java/com/example/flipkartgroceries/admin/addProducts/AddProductsFragment.kt
@@ -10,6 +10,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.flipkartgroceries.R
 import com.example.flipkartgroceries.base.BaseFragment
 import com.example.flipkartgroceries.databinding.FragmentAddProductsBinding
@@ -19,16 +20,20 @@ import kotlinx.coroutines.flow.collectLatest
 @AndroidEntryPoint
 class AddProductsFragment : BaseFragment<FragmentAddProductsBinding>() {
     override fun getLayoutResource() = R.layout.fragment_add_products
-    private val addProductsViewModel : AddProductsViewModel by viewModels()
-    val selectProductImage=registerForActivityResult(ActivityResultContracts.GetContent()){uri->
-        if (uri!=null){
-            addProductsViewModel.selectProductImage.value=uri.toString()
-        }
-        else{
-            Log.d(TAG,"Image is not available")
+    private val addProductsViewModel: AddProductsViewModel by viewModels()
+    private val args: AddProductsFragmentArgs by navArgs()
+
+    val selectProductImage =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            if (uri != null) {
+                addProductsViewModel.selectProductImage.value = uri.toString()
+                dataBinding.imageView.setImageURI(uri)
+            } else {
+                Log.d(TAG, "Image is not available")
+            }
+
         }
 
-    }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         //if ok user selected a file
@@ -47,6 +52,14 @@ class AddProductsFragment : BaseFragment<FragmentAddProductsBinding>() {
         dataBinding.viewModel = addProductsViewModel
         initToolbar()
 
+        addProductsViewModel.categoryType.value = args.details.categoryName.toString()
+        addProductsViewModel.productName.value = args.details.productName.toString()
+        addProductsViewModel.productWeight.value = args.details.productWeight.toString()
+        addProductsViewModel.productMRP.value = args.details.productMRP.toString()
+        addProductsViewModel.productPrice.value = args.details.productPrice.toString()
+        addProductsViewModel.productDescription.value = args.details.description.toString()
+        addProductsViewModel.selectProductImage.value = args.details.productImage.toString()
+
         lifecycleScope.launchWhenResumed {
             addProductsViewModel.selectImageEvent.collectLatest {
                 selectProductImage.launch("image/*")
@@ -55,50 +68,50 @@ class AddProductsFragment : BaseFragment<FragmentAddProductsBinding>() {
 
         lifecycleScope.launchWhenResumed {
             addProductsViewModel.categoryTypeError.collectLatest {
-                dataBinding.addCategoryType.error=addProductsViewModel.categoryTypeError.value
+                dataBinding.addCategoryType.error = addProductsViewModel.categoryTypeError.value
             }
         }
         lifecycleScope.launchWhenResumed {
             addProductsViewModel.productNameError.collectLatest {
-                dataBinding.addProductNameTv.error=addProductsViewModel.productNameError.value
+                dataBinding.addProductNameTv.error = addProductsViewModel.productNameError.value
             }
         }
         lifecycleScope.launchWhenResumed {
             addProductsViewModel.productWeightError.collectLatest {
-                dataBinding.productWeightTv.error=addProductsViewModel.productWeightError.value
+                dataBinding.productWeightTv.error = addProductsViewModel.productWeightError.value
             }
         }
         lifecycleScope.launchWhenResumed {
             addProductsViewModel.productMrpError.collectLatest {
-                dataBinding.productMRPTv.error=addProductsViewModel.productMrpError.value
+                dataBinding.productMRPTv.error = addProductsViewModel.productMrpError.value
             }
         }
         lifecycleScope.launchWhenResumed {
             addProductsViewModel.productPriceError.collectLatest {
-                dataBinding.productPriceTv.error=addProductsViewModel.productPriceError.value
+                dataBinding.productPriceTv.error = addProductsViewModel.productPriceError.value
             }
         }
 
 
         lifecycleScope.launchWhenResumed {
             addProductsViewModel.addProductEvent.collectLatest {
-               try {
-                   val action =
-                       AddProductsFragmentDirections.actionAddProductsFragmentToViewProductsFragment()
-                   findNavController().navigate(action)
-               }
-               catch (e:Exception){
-                   Log.d("TAG","Unable to load the image")
-               }
+                try {
+                    val action =
+                        AddProductsFragmentDirections.actionAddProductsFragmentToViewProductsFragment()
+                    findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("TAG", "Unable to load the image")
+                }
             }
         }
         lifecycleScope.launchWhenResumed {
             addProductsViewModel.toastEvent.collectLatest {
-                Toast.makeText(requireContext(),it,Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
             }
         }
     }
-    private fun initToolbar(){
+
+    private fun initToolbar() {
         dataBinding.toolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
         }
